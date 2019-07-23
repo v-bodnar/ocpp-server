5aebea913fdd524c43a61b3f236b561b1cc436f4
Volodymyr Bodnar <vbodnar@pl.sii.eu>
2019-07-23 15:59:35 +0200

    Changed GroovyService to simplify groovy part development

--- src/main/java/com/omb/ocpp/groovy/GroovyService.java
+++ src/main/java/com/omb/ocpp/groovy/GroovyService.java
@@ -4,27 +4,23 @@ import eu.chargetime.ocpp.model.Confirmation;
 import eu.chargetime.ocpp.model.Request;
 import groovy.lang.GroovyClassLoader;
 import groovy.lang.GroovyRuntimeException;
-import org.apache.tools.ant.util.FileUtils;
 import org.codehaus.groovy.control.CompilationUnit;
+import org.codehaus.groovy.control.SourceUnit;
 import org.codehaus.groovy.tools.GroovyClass;
 import org.jvnet.hk2.annotations.Service;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InvalidClassException;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.ParameterizedType;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.net.URL;
 import java.nio.channels.Channels;
 import java.nio.channels.FileChannel;
 import java.nio.channels.ReadableByteChannel;
-import java.nio.file.FileSystem;
-import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -44,14 +40,10 @@ import static com.omb.ocpp.gui.Application.LITHOS_HOME;
 @Service
 public class GroovyService {
     private static final Logger LOGGER = LoggerFactory.getLogger(GroovyService.class);
-    private static final Path SCRIPTS_FOLDER = Paths.get(LITHOS_HOME, "ocpp", "groovy","src","main","groovy","com",
-            "omb","ocpp", "groovy");
-    private static final Path SUPPLIERS_FOLDER = Paths.get(LITHOS_HOME, "ocpp", "groovy","src","main","groovy","com",
-            "omb","ocpp", "groovy", "supplier");
     private static final Path GROOVY_PROJECT_FOLDER = Paths.get(LITHOS_HOME, "ocpp", "groovy");
-    private static final String BUILD_GRADLE = "build.gradle";
-    private static final String SETTINGS_GRADLE = "settings.gradle";
-    private static final String CONFIRMATION_SUPPLIER_GROOVY_SUFFIX = "ConfirmationSupplier.groovy";
+    private static final Path SUPPLIERS_FOLDER = Paths.get(GROOVY_PROJECT_FOLDER.toString(), "src", "main", "groovy", "com",
+            "omb", "ocpp", "groovy", "supplier");
+
     private final Map<Class<? extends Request>, ConfirmationSupplier<Request, Confirmation>> confirmationSuppliers =
             new HashMap<>();
     private Consumer<Void> groovyCacheChangedListener = aVoid -> LOGGER.debug("No listeners attached");
@@ -60,71 +52,13 @@ public class GroovyService {
 
     public void loadGroovyScripts() {
         try {
-            if (!SCRIPTS_FOLDER.toFile().exists()) {
-                Files.createDirectories(SCRIPTS_FOLDER);
-
+            if (!GROOVY_PROJECT_FOLDER.toFile().exists()) {
+                throw new FileNotFoundException(String.format("Please execute \"git clone https://github" +
+                        ".com/v-bodnar/GroovyOcppSupplier.git %s \"", GROOVY_PROJECT_FOLDER));
             }
-            createGroovyFilesFromResources();
             reloadGroovyFiles();
-        } catch (IOException | URISyntaxException e) {
+        } catch (IOException e) {
             LOGGER.error("Could not load groovy scripts", e);
-        }
-    }
-
-    private void createGroovyFilesFromResources() throws IOException, URISyntaxException {
-        URL innerResourceFolderUrl = ClassLoader.getSystemClassLoader().getResource("groovy");
-        URI gradleBuild =
-                Optional.ofNullable(ClassLoader.getSystemClassLoader().getResource("groovy/" + BUILD_GRADLE))
-                        .orElseThrow().toURI();
-        URI settingsBuild =
-                Optional.ofNullable(ClassLoader.getSystemClassLoader().getResource("groovy/" + SETTINGS_GRADLE))
-                        .orElseThrow().toURI();
-
-        if (innerResourceFolderUrl == null) {
-            LOGGER.error("Could not find groovy scripts in resources");
-            return;
-        }
-
-        LOGGER.debug("build.gradle path: {}", gradleBuild.toString());
-        LOGGER.debug("settings.gradle path: {}", gradleBuild.toString());
-
-        // -------- __@      __@       __@       __@      __~@
-        // ----- _`\<,_    _`\<,_    _`\<,_     _`\<,_    _`\<,_
-        // ---- (*)/ (*)  (*)/ (*)  (*)/ (*)  (*)/ (*)  (*)/ (*)
-        // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-        //FIXME ugly warkaround for problem with getting resources from jar
-        final URI uri = Optional.of(innerResourceFolderUrl.toURI())
-                .orElseThrow(() -> new IOException(String.format("Could not create URI to file %s", innerResourceFolderUrl)));
-        if (uri.toString().contains("!")) {
-            final String[] array = uri.toString().split("!");
-            try (final FileSystem fs = FileSystems.newFileSystem(URI.create(array[0]), new HashMap<>());
-                 Stream<Path> stream = Files.walk(fs.getPath(array[1]))) {
-                stream.filter(path -> (path.toString().endsWith(CONFIRMATION_SUPPLIER_GROOVY_SUFFIX) ||
-                        path.toString().endsWith(BUILD_GRADLE)) && Files.isRegularFile(path))
-                        .forEach(path -> createGroovyFile(path,
-                                Paths.get(SUPPLIERS_FOLDER.toString(), path.getFileName().toString())));
-            }
-        } else {
-            try (Stream<Path> stream = Files.walk(Paths.get(uri))) {
-                stream.filter(path -> path.toString().endsWith(CONFIRMATION_SUPPLIER_GROOVY_SUFFIX) && Files.isRegularFile(path) )
-                        .forEach(path -> createGroovyFile(path,
-                                Paths.get(SUPPLIERS_FOLDER.toString(), path.getFileName().toString())));
-            }
-            createGroovyFile(Paths.get(gradleBuild), GROOVY_PROJECT_FOLDER.resolve(BUILD_GRADLE));
-            createGroovyFile(Paths.get(settingsBuild), GROOVY_PROJECT_FOLDER.resolve(SETTINGS_GRADLE));
-        }
-        // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    }
-
-    private void createGroovyFile(Path source, Path destination) {
-        if (!Files.exists(destination) && Files.isRegularFile(source)) {
-            LOGGER.debug("Creating groovy file: {}", destination);
-            try (ReadableByteChannel src = Channels.newChannel(Files.newInputStream(source));
-                 FileChannel dest = new FileOutputStream(destination.toFile()).getChannel()) {
-                dest.transferFrom(src, 0, Integer.MAX_VALUE);
-            } catch (IOException e) {
-                LOGGER.error(String.format("Could not create scripts file %s", source.getFileName()), e);
-            }
         }
     }
 
@@ -144,7 +78,7 @@ public class GroovyService {
     }
 
     public void uploadGroovyScript(InputStream inputStream, String scriptName) throws Exception {
-        Path destination = Paths.get(SCRIPTS_FOLDER.toString(), scriptName);
+        Path destination = Paths.get(SUPPLIERS_FOLDER.toString(), scriptName);
         LOGGER.debug("Replacing file {}", destination);
         Files.deleteIfExists(destination);
         try (ReadableByteChannel src = Channels.newChannel(inputStream);
@@ -166,22 +100,43 @@ public class GroovyService {
 
     private List<Class> compileGroovyFiles() {
         List<Class> compiledClasses = new LinkedList<>();
+        List<SourceUnit> sourceUnits = new LinkedList<>();
         CompilationUnit compileUnit = new CompilationUnit(groovyClassLoader);
-        try (Stream<Path> stream = Files.walk(SCRIPTS_FOLDER)) {
+        try (Stream<Path> stream = Files.walk(GROOVY_PROJECT_FOLDER)) {
             stream.filter(path -> path.toString().endsWith(".groovy") && Files.isRegularFile(path))
                     .forEach(path -> {
                         LOGGER.debug("Adding file for compilation: {}", path);
-                        compileUnit.addSource(path.toFile());
+                        sourceUnits.add(compileUnit.addSource(path.toFile()));
                     });
         } catch (IOException | GroovyRuntimeException e) {
-            LOGGER.error(String.format("Can't walk path: %s", SCRIPTS_FOLDER), e);
+            LOGGER.error(String.format("Can't walk path: %s", GROOVY_PROJECT_FOLDER), e);
         }
         compileUnit.compile();
 
         for (Object compileClass : compileUnit.getClasses()) {
             GroovyClass groovyClass = (GroovyClass) compileClass;
             byte[] compiledScriptBytes = groovyClass.getBytes();
-            compiledClasses.add(compileUnit.getClassLoader().defineClass(groovyClass.getName(), compiledScriptBytes));
+            try {
+                Class clazz = compileUnit.getClassLoader().loadClass(groovyClass.getName());
+                throw new FeatureNotSupportedException("Dynamic groovy reload is not supported yet"); //todo
+//                sourceUnits.stream()
+//                        .filter(sourceUnit1 -> sourceUnit1.getName().endsWith(String.format("%s.groovy",
+//                                clazz.getSimpleName())))
+//                        .findFirst().ifPresentOrElse(sourceUnit1 -> {
+//                            try {
+//                                compiledClasses.add(compileUnit.getClassLoader().parseClass(sourceUnit1.getSource().getReader(),
+//                                        sourceUnit1.getSource().getURI().toString()));
+//                            } catch (IOException e) {
+//                                LOGGER.error(String.format("Can't reload class %s", groovyClass.getName()), e);
+//                            }
+//                        },
+//                        () -> LOGGER.error("Can't reload class {}", groovyClass.getName()));
+
+            } catch (ClassNotFoundException e) {
+                compiledClasses.add(compileUnit.getClassLoader().defineClass(groovyClass.getName(), compiledScriptBytes));
+            } catch (FeatureNotSupportedException e) {
+                LOGGER.error(e.getMessage());
+            }
         }
 
         return compiledClasses;
@@ -225,5 +180,23 @@ public class GroovyService {
 
     public void setGroovyCacheChangedListener(Consumer<Void> groovyCacheChangedListener) {
         this.groovyCacheChangedListener = groovyCacheChangedListener;
+    }
+
+    class FeatureNotSupportedException extends Exception{
+        public FeatureNotSupportedException(String message) {
+            super(message);
+        }
+
+        public FeatureNotSupportedException(String message, Throwable cause) {
+            super(message, cause);
+        }
+
+        public FeatureNotSupportedException(Throwable cause) {
+            super(cause);
+        }
+
+        public FeatureNotSupportedException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+            super(message, cause, enableSuppression, writableStackTrace);
+        }
     }
 }
